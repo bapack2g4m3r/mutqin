bf4114b68896b35adac35ab0ec4afd3b8b17f93e
Agung Sulistiono <agung5s7@agung5s7-MacBook-Pro.local>
2026-08-19 17:39:35 +0700

    fix: export name for proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export async function middleware(request: NextRequest) {
+export default async function proxy(request: NextRequest) {
   let isMaintenanceMode = false;
   
   try {
